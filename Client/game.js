7c4b1057da723aff205e4a81e9a0a58923af71e9
Christian Maniyar <christian.maniyar@cas.de>
2017-05-10 19:49:25 +0200

    Kleines Refactoring

--- Client/game.js
+++ Client/game.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	var game = {
-		fields: ["a1","a2","a3","a4","a5","b1","b2","b3","b4","b5","c1","c2","c3","c4","c5","d1","d2","d3","d4","d5","e1","e2","e3","e4","e5",],
+		fields: ["a1","a2","a3","a4","a5","b1","b2","b3","b4","b5","c1","c2","c3","c4","c5","d1","d2","d3","d4","d5","e1","e2","e3","e4","e5"],
 		levels: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 		workers1: [],
 		workers2: [],
@@ -9,32 +9,14 @@ $(document).ready(function(){
 			game.clickHandlers();
 		},
 		initBoard: function() {
-			for(i = 0; i < game.levels.length; i ++) {
+			for(i = 0; i < game.fields.length; i++) {
 				$('.board').append('<div id="field'+i+'" class="field field'+i+' level'+game.levels[i]+'"><div id="worker" class="worker"></div></div>');
 			}
 		},
 		updateBoard: function() {
-			for(i = 0; i < game.levels.length; i ++) {
-				$('.field'+i).removeClass('level0');
-				$('.field'+i).removeClass('level1');
-				$('.field'+i).removeClass('level2');
-				$('.field'+i).removeClass('level3');
-				$('.field'+i).removeClass('level4').addClass('level'+game.levels[i]);
-			}
-			for(i = 0; i < game.fields.length; i ++) {
-				$('.field'+i).find('.worker').removeClass('worker1');
-			}
-			for(i = 0; i < game.workers1.length; i ++) {
-				var index = game.getIndex(game.workers1[i]);
-				$('.field'+index).find('.worker').addClass('worker1');
-			}
-			for(i = 0; i < game.fields.length; i ++) {
-				$('.field'+i).find('.worker').removeClass('worker2');
-			}
-			for(i = 0; i < game.workers2.length; i ++) {
-				var index = game.getIndex(game.workers2[i]);
-				$('.field'+index).find('.worker').addClass('worker2');
-			}
+			game.updateLevels();
+			game.updateWorkers(game.workers1, 'worker1');
+			game.updateWorkers(game.workers2, 'worker2');
 		},
 		loadGame: function() {
 			$.ajax({
@@ -45,7 +27,7 @@ $(document).ready(function(){
 				'success': function (data) {
 					game.levels = data.levels;
 					game.workers1 = data.workers.player1;
-					game.workers2 = data.workers.player2;					
+					game.workers2 = data.workers.player2;	
 				}
 			});
 		},
@@ -55,10 +37,33 @@ $(document).ready(function(){
 				game.updateBoard();
 			});
 		},
+		updateLevels: function() {		
+			for(field = 0; field < game.fields.length; field++) {
+				game.removeLevels('.field'+field);
+				game.setLevel('.field'+field, game.levels[field]);
+			}
+		},
+		removeLevels: function(field) {
+			for(level = 0; level < 5; level++) {
+				$(field).removeClass('level'+level);	
+			}
+		},
+		setLevel: function(field, level) {
+			$(field).addClass('level'+level);
+		},
+		updateWorkers: function(workers, cls) {
+			for(field = 0; field < game.fields.length; field++) {
+				$('.field'+field).find('.worker').removeClass(cls);
+			}
+			for(field = 0; field < workers.length; field++) {
+				index = game.getIndex(workers[field]);
+				$('.field'+index).find('.worker').addClass(cls);
+			}
+		},
 		getIndex: function(field) {
-			for(i = 0; i < game.fields.length; i ++) {
-				if (game.fields[i] == field) {
-					return i;
+			for(j = 0; j < game.fields.length; j ++) {
+				if (game.fields[j] == field) {
+					return j;
 				}
 			}
 			return -1;
